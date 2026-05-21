2af8eee35f84fa81b3554ac31fda789621edc380
Ellis-Castillo <ellis.c.schmuck@gmail.com>
2026-05-21 10:12:19 +0200

    Primeras carpetas

---
+++ backend/cineapp/src/test/java/com/example/cineapp/CineappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cineapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
